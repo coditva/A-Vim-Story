5c57ece55b85f4c16331c9461fbe3e75ad6ba948
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-16 01:38:16 +0530

    Allow single digit numbers in commands

--- src/action.c
+++ src/action.c
@@ -1,34 +1,55 @@
+#include <assert.h>
+
 #include "datatypes.h"
 #include "action.h"
 #include "map.h"
+#include "input.h"
 
 void action_make_move(map_t *map, input_key_t key)
 {
     point_t point;
+    int loop = 1;
 
     /* check if key's available */
 
     /* calculate the update */
     point.x = map -> cursor.x;
     point.y = map -> cursor.y;
-    switch (key) {
-        case 'j':
-            point.y = map -> cursor.y + 1;
-            break;
-        case 'k':
-            point.y = map -> cursor.y - 1;
-            break;
-        case 'l':
-            point.x = map -> cursor.x + 1;
-            break;
-        case 'h':
-            point.x = map -> cursor.x - 1;
-            break;
-    }
 
-    /* check if map is free and update the map */
-    if (map_is_free(map, point)) {
-        map -> cursor.x = point.x;
-        map -> cursor.y = point.y;
+    while (loop--) {
+        switch (key) {
+            case 'j':
+                point.y += 1;
+                break;
+            case 'k':
+                point.y -= 1;
+                break;
+            case 'l':
+                point.x += 1;
+                break;
+            case 'h':
+                point.x -= 1;
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                loop = key - '0';
+                key = input_get_key();
+                break;
+        }
+
+        if (!map_is_free(map, point)) {
+            break;
+        } else {
+            map -> cursor.x = point.x;
+            map -> cursor.y = point.y;
+        }
     }
 }
