a718552a6f1b6e572f8cb1a457c4f54d84aa3099
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-20 21:08:47 +0530

    Add a signal handler to exit cleanly

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
+#include <signal.h>             /* for signal() */
+#include <stdlib.h>
+
 #include "datatypes.h"
 #include "game.h"
 #include "interface.h"
 #include "menu.h"
 
+void signal_handler(int);
+
 int main(int argc, char *argv[])
 {
     enum menu_item choice = -1;
+
+    /* attach handler to signals */
+    signal(SIGINT , signal_handler);
+    signal(SIGKILL, signal_handler);
+    signal(SIGSTOP, signal_handler);
 
     interface_init();
 
@@ -26,4 +36,10 @@ int main(int argc, char *argv[])
     interface_destroy();
 
     return 0;
+}
+
+void signal_handler(int signal_number)
+{
+    interface_destroy();
+    exit(1);
 }
