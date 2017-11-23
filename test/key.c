c68b6ad76ab6134df9f08f76e201b3f15491fec2
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-23 06:45:09 +0530

    Add tests for `key` and `msg` modules

--- test/key.c
+++ test/key.c
@@ -3,7 +3,13 @@
 #include "key.h"
 
 Test(key, lock_all) {
+    key_unlock_all();
     key_lock_all();
+    cr_expect(!key_unlocked('a'), "Key should be locked when all locked");
+}
 
-    cr_expect(!key_unlocked('a'), "Key should be locked when locked");
+Test(key, unlock_all) {
+    key_lock_all();
+    key_unlock_all();
+    cr_expect(key_unlocked('a'), "Key should be unlocked when all unlocked");
 }
